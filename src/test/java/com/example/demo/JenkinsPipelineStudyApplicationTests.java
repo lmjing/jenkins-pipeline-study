ae3a6006ec037ba9ea2ee145f5af1c6d33dcfbb9
lmjing <lmjing1234@gmail.com>
2020-03-02 10:17:35 +0900

    프로젝트 생성

---
+++ src/test/java/com/example/demo/JenkinsPipelineStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsPipelineStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
