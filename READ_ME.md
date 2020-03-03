ddf85052bac37997769bcb89b93a3b830af35f16
lmjing <lmjing1234@gmail.com>
2020-03-03 18:47:36 +0900

    READ_ME 추가

---
+++ READ_ME.md
@@ -0,0 +1,4 @@
+# jenkins pipeline 구성
+1. docker - jenkins 연동
+2. jenkins - github 연동
+3. jenkins build trigger 설정 (git 연동 테스트)
---
+++ src/main/java/com/example/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Maven Hello World!");
+	}
+}
